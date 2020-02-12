14984d4a4e034a5297570c1b21a546a75aac7c48
ElhadiCherifi95 <dadycitroen@gmail.com>
2020-02-12 17:30:04 +0100

    Add tests

--- src/test/java/com/github/elhadicherifi95/kafka/kafkastreamtests/StreamsBuzzFeedTests.java
+++ src/test/java/com/github/elhadicherifi95/kafka/kafkastreamtests/StreamsBuzzFeedTests.java
@@ -9,13 +9,17 @@ import org.junit.Test;
 
 import java.util.Properties;
 
-import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class StreamsBuzzFeedTests {
 
     TopologyTestDriver testDriver;
-    private TestInputTopic<String, String> inputTopic;
-    private TestOutputTopic<String, Long> outputTopic;
+    private TestInputTopic<String, String> buzzFeedTopic;
+    private TestOutputTopic<String, String> buzzFeedPredicateTopic;
+    private TestOutputTopic<String, String> buzzFeedMapperTopic;
+    private TestOutputTopic<String, Long> buzzFeedCounterTopic;
 
     private Serde<String> stringSerde = new Serdes.StringSerde();
     private Serde<Long> longSerde = new Serdes.LongSerde();
@@ -33,8 +37,10 @@ public class StreamsBuzzFeedTests {
         testDriver = new TopologyTestDriver(topology, props);
 
         // setup test topics
-        inputTopic = testDriver.createInputTopic("input-topic", stringSerde.serializer(), stringSerde.serializer());
-        outputTopic = testDriver.createOutputTopic("result-topic", stringSerde.deserializer(), longSerde.deserializer());
+        buzzFeedTopic = testDriver.createInputTopic("buzz-feed", stringSerde.serializer(), stringSerde.serializer());
+        buzzFeedPredicateTopic = testDriver.createOutputTopic("buzz-feed-predicate", stringSerde.deserializer(), stringSerde.deserializer());
+        buzzFeedMapperTopic = testDriver.createOutputTopic("buzz-feed-mapper", stringSerde.deserializer(), stringSerde.deserializer());
+        buzzFeedCounterTopic = testDriver.createOutputTopic("buzz-feed-counter", stringSerde.deserializer(), longSerde.deserializer());
 
     }
 
@@ -45,8 +51,43 @@ public class StreamsBuzzFeedTests {
 
 
     @Test
-    public void dummuyTest(){
-        String dummy = "Du" + "mmy";
-        assertEquals(dummy, "Dummy");
+    public void predicateTestor(){
+        buzzFeedTopic.pipeInput("key", "1");
+        buzzFeedTopic.pipeInput("key", "abcd");
+        assertThat(buzzFeedPredicateTopic.readKeyValue(), equalTo(new KeyValue<>("key", "1")));
+        assertTrue(buzzFeedPredicateTopic.isEmpty());
+    }
+
+    @Test
+    public void mapperTestor(){
+        buzzFeedTopic.pipeInput("key", "1");
+        buzzFeedTopic.pipeInput("key", "3");
+        buzzFeedTopic.pipeInput("key", "5");
+        assertThat(buzzFeedMapperTopic.readKeyValue(), equalTo(new KeyValue<>("key", "buzzfeed")));
+        assertThat(buzzFeedMapperTopic.readKeyValue(), equalTo(new KeyValue<>("key", "buzz")));
+        assertThat(buzzFeedMapperTopic.readKeyValue(), equalTo(new KeyValue<>("key", "feed")));
+        assertTrue(buzzFeedMapperTopic.isEmpty());
+    }
+
+    @Test
+    public void counterTestor(){
+        buzzFeedTopic.pipeInput("key", "1");
+        buzzFeedTopic.pipeInput("key", "9");
+        buzzFeedTopic.pipeInput("key", "3");
+        buzzFeedTopic.pipeInput("key", "5");
+        buzzFeedTopic.pipeInput("key", "20");
+        buzzFeedTopic.pipeInput("key", "3");
+        buzzFeedTopic.pipeInput("key", "1");
+
+
+        assertThat(buzzFeedCounterTopic.readKeyValue(), equalTo(new KeyValue<>("buzzfeed", 1L)));
+        assertThat(buzzFeedCounterTopic.readKeyValue(), equalTo(new KeyValue<>("buzz", 1L)));
+        assertThat(buzzFeedCounterTopic.readKeyValue(), equalTo(new KeyValue<>("buzz", 2L)));
+        assertThat(buzzFeedCounterTopic.readKeyValue(), equalTo(new KeyValue<>("feed", 1L)));
+        assertThat(buzzFeedCounterTopic.readKeyValue(), equalTo(new KeyValue<>("feed", 2L)));
+        assertThat(buzzFeedCounterTopic.readKeyValue(), equalTo(new KeyValue<>("buzz", 3L)));
+        assertThat(buzzFeedCounterTopic.readKeyValue(), equalTo(new KeyValue<>("buzzfeed", 2L)));
+        assertTrue(buzzFeedCounterTopic.isEmpty());
+
     }
 }
